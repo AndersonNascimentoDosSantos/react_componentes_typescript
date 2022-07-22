aaf9625887b5026677b79c8be00e2189d7b8b475
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2022-07-22 09:01:49 -0300

    feat: post container initial setup

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { PostCardProps } from "../interface";
+import { ReverseCategoryTag } from "./ReverseCategoryTag";
 
-// import { Container } from './styles';
+import { PostContainer, PostInfo } from "./styles";
 
-const PostCard: React.FC = () => {
-  return <div />;
-}
+const PostCard: React.FC<PostCardProps> = ({
+  href,
+  imageSrc,
+  title,
+  subtitle,
+}) => {
+  return (
+    <PostContainer direction="column" height="150px" width="150px">
+      <div>
+        <picture>
+          <Link to={href} rel="noopener noreferrer">
+            <img src={imageSrc} alt={title} />
+          </Link>
+        </picture>
+        <section>
+          <PostInfo>
+            {/* <span>{date ? formatDateNotHour(date) : ''}</span> */}
+            <span>21 de abril de 2022</span>
+          </PostInfo>
+        </section>
+      </div>
+      <div>
+        <Link to={href}>
+          <a target="_blank" rel="noopener noreferrer">
+            <h2>{title}</h2>
+          </a>
+        </Link>
+        <p>{subtitle}</p>
+        <div>
+          {/* {!isScreenLessThan768 && (
+            <ShareButtonFocus
+              size={24}
+              title={title}
+              url={createUriLink(href)}
+              stylesComponent={{ color: '#000' }}
+              darkIcon
+            />
+          )} */}
+
+          <Link to={href}>
+            <a>
+              <ReverseCategoryTag text="veja mais" isBold />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </PostContainer>
+  );
+};
 
 export default PostCard;
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,3 +1,56 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const PostContainer = styled.div``;
+export const PostContainer = styled.div<{
+  reverseContentPosition?: boolean;
+  direction: "row" | "column";
+  height: string;
+  width: string;
+}>`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  gap: 40px;
+  border: solid 1px red;
+
+  ${(props) =>
+    props.reverseContentPosition &&
+    css`
+      flex-direction: ${props.direction}-reverse;
+    `}
+  >div {
+    flex: 4;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    picture {
+      width: ${(props) => props.width};
+      width: ${(props) => props.height};
+      a {
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
+`;
+
+export const PostInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  span {
+    font-size: 0.75rem;
+    line-height: 0.875rem;
+    font-weight: 500;
+    color: #808080;
+  }
+
+  @media (max-width: 768px) {
+    gap: 0;
+
+    span {
+      font-size: 0.625rem;
+    }
+  }
+`;
