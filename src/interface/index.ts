eb847ff9b34034b9a1741ec0ab6fa4f8417b4e47
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2022-07-22 09:05:41 -0300

    feat: postCard interface

---
+++ src/interface/index.ts
@@ -0,0 +1,12 @@
+export interface PostCardProps {
+  reverseContentPosition?: boolean;
+  href: string;
+  title: string;
+  subtitle: string;
+  date: string;
+  imageSrc: string;
+  category?: string;
+  authorName: string;
+  cod: number;
+  isHome?: boolean;
+}
