07b7a9e75f36b4fedeffc1a666eb4cb7b4238bd4
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2022-07-22 09:00:33 -0300

    feat: routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-import "./App.css";
 import { ThemeProvider } from "styled-components";
+import Router from "./router";
 import theme from "./theme";
+import GlobalStyle from "./theme/global";
 
 function App() {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
